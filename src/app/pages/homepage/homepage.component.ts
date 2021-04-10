25547865da627c05171574a778ef2bc3763a8c94
Jessica Nierth <jnierth@web.de>
2021-04-10 18:37:15 +0200

    feat(menu): added main menu to homepage

--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {AuthService} from "../../services/auth/auth.service";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.scss']
 })
-export class HomepageComponent implements OnInit {
+export class HomepageComponent implements OnInit, OnDestroy {
+  public isAuthenticated = false;
+  private userSub: Subscription;
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
+    this.userSub = this.authService.user.subscribe(user => {
+      this.isAuthenticated = !!user;
+      console.log('this.auth: ', this.isAuthenticated)
+    });
   }
 
+  ngOnDestroy() {
+    this.userSub.unsubscribe();
+  }
 }
