78a0e91d300b0ad259e8663b0f291f0c871d6dc2
Jessica Nierth <jnierth@web.de>
2021-05-08 20:46:59 +0200

    fix(sticky table header): Added sticky header to table

--- src/app/services/speaker/speakers.fixture.ts
+++ src/app/services/speaker/speakers.fixture.ts
@@ -1,7 +1,7 @@
 import { Speaker, Speakers } from "./speaker.model";
 
 export const speakerFixture:Speakers = {
-  "MZ3dAbC2GX5g8iQMMl5": {
+  "-MZ3dAbC2GX5g8iQMMl5": {
     id: "MZ3dAbC2GX5g8iQMMl5",
     firstname: 'Andreas',
     lastname: 'Fröhlich',
@@ -9,8 +9,41 @@ export const speakerFixture:Speakers = {
       {name: "A. Fröhlich"}
     ]
   },
-  "MZ3dAbC2GX5g8iQMMl3": {
+  "-MZ3dAbC2GX5g8iQMMl3": {
       id: "MZ3dAbC2GX5g8iQMMl3",
+      firstname: 'Hans',
+      lastname: 'Paetsch',
+      aliases: [
+        {name: "H. Paetsch"}
+      ]
+    },
+  "-MZ3dAbe2GX5g8iQMMl5": {
+      id: "MZ3dAbe2GX5g8iQMMl5",
+      firstname: 'Bert',
+      lastname: 'Bart',
+      aliases: [
+        {name: "B. Bart"},
+        {name: "B. Barti"},
+      ]
+    },
+  "-MZ3eAbC2GX5g8idMMl3": {
+      id: "MZ3eAbC2GX5g8idMMl3",
+      firstname: 'Hansi',
+      lastname: 'Gaetsch',
+      aliases: [
+        {name: "H. Gaetsch"}
+      ]
+    },
+  "-DZ3dAbC2Gsdg8iQMMl3": {
+      id: "DZ3dAbC2Gsdg8iQMMl3",
+      firstname: 'Hanse',
+      lastname: 'Paetsche',
+      aliases: [
+        {name: "H. Paetsche"}
+      ]
+    },
+  "-MZ3dAbC2GXeg8iQgMl3": {
+      id: "MZ3dAbC2GXeg8iQgMl3",
       firstname: 'Hans',
       lastname: 'Paetsch',
       aliases: [
