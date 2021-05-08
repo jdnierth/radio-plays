98d2ddd1816ee8a1020740b1345a1cfe8f245eb8
Jessica Nierth <jnierth@web.de>
2021-05-08 18:40:19 +0200

    fix(speaker parent rout): making sure first child route will always be shown on parent route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -21,6 +21,11 @@ const routes: Routes = [
     canActivate: [ AuthGuardService ],
     children: [
       {
+        path: '',
+        redirectTo: 'list',
+        pathMatch: 'prefix'
+      },
+      {
         path: 'import',
         component: SpeakerImportComponent
       },
